16537ea72bcf5621de17227ed4302779d088a104
Luc Sinet <luc.sinet@epitech.eu>
2014-05-03 14:57:22 +0200

    made simple queue functions

---
+++ libsocket/src/queue.c
@@ -0,0 +1,82 @@
+/*
+** send_answer.c for send_answer in /home/sinet_l/Documents/project/PSU_2013_myirc/src/fserver
+**
+** Made by luc sinet
+** Login   <sinet_l@epitech.net>
+**
+** Started on  Sun Apr 20 08:27:21 2014 luc sinet
+** Last update Sat May  3 14:32:56 2014 luc sinet
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include "queue.h"
+
+int		add_to_queue(t_queue **queue, char *msg)
+{
+  t_queue	*tmp;
+  t_queue	*new;
+
+  tmp = *queue;
+  if ((new = malloc(sizeof(t_queue))) == NULL ||
+      (new->msg = strdup(msg)) == NULL)
+    {
+      perror("add_to_queue: malloc");
+      return (-1);
+    }
+  new->next = NULL;
+  if (!*queue)
+    {
+      *queue = new;
+      return (0);
+    }
+  while (tmp->next)
+    tmp = tmp->next;
+  tmp->next = new;
+  return (0);
+}
+
+void		pop_msg(t_queue **queue)
+{
+  t_queue	*tmp;
+
+  tmp = *queue;
+  if (tmp == NULL)
+    return ;
+  free(tmp->msg);
+  *queue = tmp->next;
+  free(tmp);
+}
+
+void	shift_msg(char *msg, int pos)
+{
+  int	i;
+
+  i = 0;
+  while (msg[pos])
+    msg[i++] = msg[pos++];
+  msg[i] = '\0';
+}
+
+void		erase_queue(t_queue **queue)
+{
+  t_queue	*tmp;
+  t_queue	*next;
+
+  tmp = *queue;
+  while (tmp)
+    {
+      next = tmp->next;
+      free(tmp->msg);
+      free(tmp);
+      tmp = next;
+    }
+  *queue = NULL;
+}
+
+t_queue	*init_queue()
+{
+  return (NULL);
+}
