74f965f9ea9aace40d9bd2c3d76f93fb0155af7d
Guillaume Fillon <guillaume@posteo.de>
2014-06-16 10:34:06 +0200

    Change serv to server (more explicit)

--- server/src/pars.c
+++ server/src/pars.c
@@ -5,14 +5,14 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Fri May  2 22:46:12 2014 luc sinet
-** Last update Fri May  2 23:13:24 2014 luc sinet
+** Last update Mon Jun 16 10:19:05 2014 guillaume fillon
 */
 
 #include "server.h"
 
-int	pars_msg(t_serv *serv, t_client *cl, char *cmd)
+int	pars_msg(t_server *server, t_client *cl, char *cmd)
 {
-  (void)serv;
+  (void)server;
   (void)cl;
   write(1, "Got message ", strlen("Got message "));
   write(1, cmd, strlen(cmd));
