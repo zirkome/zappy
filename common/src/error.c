4f8a2ea9c4ad6c371e7eeb7a1542ccd3daaeb014
Guillaume Fillon <guillaume@posteo.de>
2014-06-16 10:59:55 +0200

    Use warn instead of perror (GNU error handling compliant)

--- common/src/error.c
+++ common/src/error.c
@@ -5,31 +5,32 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Mon Apr 14 10:22:50 2014 luc sinet
-** Last update Fri May  2 09:50:53 2014 luc sinet
+** Last update Mon Jun 16 10:57:35 2014 guillaume fillon
 */
 
 #include <stdio.h>
+#include <err.h>
 
 int	ierror(const char *s, int ret)
 {
-  fprintf(stderr, s);
+  fprintf(stderr, "%s\n", s);
   return (ret);
 }
 
 void	*pterror(const char *s, void *ret)
 {
-  fprintf(stderr, s);
+  fprintf(stderr, "%s\n", s);
   return (ret);
 }
 
 int	iperror(const char *func, int ret)
 {
-  perror(func);
+  warn("%s", func);
   return (ret);
 }
 
 void	*ptperror(const char *func, void *ret)
 {
-  perror(func);
+  warn("%s", func);
   return (ret);
 }
