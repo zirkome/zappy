be9d6bd04d207f9cef0557b8a766c065fc14fa9a
Guillaume Fillon <guillaume@posteo.de>
2014-06-26 13:39:56 +0200

    Add overloading test script

---
+++ load_test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+max=100
+ip="$1"
+port="$2"
+file="$3"
+
+if [ "${#ip}" -eq 0 ]
+then
+    ip="127.0.0.1"
+fi
+
+if [ "${#port}" -eq 0 ]
+then
+    port="6000"
+fi
+
+if [ "${#file}" -eq 0 ]
+then
+    file="1MB"
+fi
+
+for i in `seq 1 $max`
+do
+    nc "$ip" "$port" < $file > /dev/null &
+done
