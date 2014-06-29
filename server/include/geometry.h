8b288810026cc3fbda7fa7c803244fe038a71797
Luc Sinet <luc.sinet@epitech.eu>
2014-06-30 00:07:23 +0200

    now get the case from where the message is received

--- server/include/geometry.h
+++ server/include/geometry.h
@@ -5,11 +5,13 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Sat Jun 28 18:02:43 2014 luc sinet
-** Last update Sun Jun 29 13:28:32 2014 luc sinet
+** Last update Sun Jun 29 22:57:34 2014 luc sinet
 */
 
 #ifndef _GEOMETRY_H_
 # define _GEOMETRY_H_
+
+# define GEO_STEP 1.499998
 
 typedef struct	s_geometry
 {
@@ -21,6 +23,12 @@ typedef struct	s_geometry
   double	a;
   double	b;
 }		t_geometry;
+
+typedef	struct	s_case
+{
+  double	bx;
+  double	by;
+}		t_case;
 
 double	point_distance(double ax, double ay, double bx, double by);
 double	calc_director_coef(double ax, double ay, double bx, double by);
--- server/src/player_broadcast.c
+++ server/src/player_broadcast.c
@@ -5,7 +5,7 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Mon Jun 23 10:40:26 2014 luc sinet
-** Last update Sun Jun 29 13:44:54 2014 luc sinet
+** Last update Sun Jun 29 23:59:23 2014 luc sinet
 */
 
 #include "geometry.h"
@@ -39,21 +39,84 @@ void	translate_point(t_geometry *geo, t_world *world, int dist)
     }
 }
 
-int		find_player_dir(t_player *pla, t_player *plb, t_world *world)
+int		find_case(t_geometry *geo, double *x, double *y)
+{
+  double	dist;
+  double	tmp_dist;
+  double	tx;
+  double	ty;
+
+  dist = -1;
+  tx = (geo->bx + ((geo->bx > geo->ax) ? -GEO_STEP : GEO_STEP));
+  ty = geo->by;
+  dist = point_distance(geo->ax, geo->ay, tx, ty);
+  *x = geo->bx;
+  *y = (geo->by + ((geo->by > geo->ay) ? -GEO_STEP : GEO_STEP));
+  tmp_dist = point_distance(geo->ax, geo->ay, *x, *y);
+  if (dist < tmp_dist)
+    {
+      *x = tx;
+      *y = ty;
+      return (0);
+    }
+  return (1);
+}
+
+int		get_case_pos(double ax, double ay,
+			     double x, double y)
+{
+  static	t_case tab[8] = {{0.5, -1.5},
+				 {-0.5, -1.5},
+				 {-0.5, -0.5},
+				 {-0.5, 0.5},
+				 {0.5, -0.5},
+				 {1.5, 0.5},
+				 {1.5, -0.5},
+				 {1.5, -1.5}};
+  int		i;
+
+  for (i = 0; i < 8; ++i)
+    {
+      if (y > tab[i].by + ay && y < tab[i].by + 1 + ay &&
+	  x > tab[i].bx - 1 + ax && x < tab[i].bx + ax)
+	return (i + 1);
+    }
+  return (0);
+}
+
+int		send_broadcast(t_geometry *geo, t_client *client, char *arg)
+{
+  /* char		tab[64]; */
+  int		pos;
+  double	x;
+  double	y;
+
+  if (find_case(geo, &x, &y) == 0)
+    y = (geo->a * x + geo->b);
+  else
+    x = ((y - geo->b) / geo->a);
+  pos = get_case_pos(geo->bx, geo->by, x, y);
+  pos = (pos + client->player->dir * 2) % 9;
+  //  snprintf(tab, 64, "message %d,", pos);
+  return (0);
+}
+
+int		find_player_dir(t_player *pla, t_client *client,
+				t_world *world, char *arg)
 {
   t_geometry	geo;
+  t_player	*plb;
 
-  geo.ax = pla->x;
-  geo.ay = pla->y;
-  geo.bx = plb->x;
-  geo.by = plb->y;
+  plb = client->player;
+  geo.ax = pla->x + 0.5;
+  geo.ay = pla->y + 0.5;
+  geo.bx = plb->x + 0.5;
+  geo.by = plb->y + 0.5;
   geo.p_dist = point_distance(geo.ax, geo.ay, geo.bx, geo.by);
   translate_point(&geo, world, 0);
   geo.a = calc_director_coef(geo.ax, geo.ay, geo.bx, geo.by);
   geo.b = get_origin_coor(geo.ax, geo.ay, geo.a);
-  printf("A[%f, %f], B[%f, %f], y = %fx + %f\n",
-	 geo.ax, geo.ay, geo.bx, geo.by, geo.a, geo.b);
-  return (0);
+  return (send_broadcast(&geo, client, arg));
 }
 
 int		pl_broadcast(t_server *server, t_client *client,
@@ -65,6 +128,6 @@ int		pl_broadcast(t_server *server, t_client *client,
   pl = client->player;
   for (tmp = server->cl; tmp != NULL; tmp = tmp->next)
     if (tmp != client)
-      find_player_dir(pl, tmp->player, &server->world);
+      find_player_dir(pl, tmp, &server->world, arg);
   return (0);
 }
