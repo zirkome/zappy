6b8f09e2ec66464bca1005b4ab950007bc54a477
Guillaume Fillon <guillaume@posteo.de>
2014-06-30 17:42:07 +0200

    Add plv (player level)

--- server/src/gui_player.c
+++ server/src/gui_player.c
@@ -5,7 +5,7 @@
 ** Login   <kokaz@epitech.net>
 **
 ** Started on  Tue Jun 24 13:50:43 2014 guillaume fillon
-** Last update Mon Jun 30 17:28:01 2014 guillaume fillon
+** Last update Mon Jun 30 17:42:01 2014 guillaume fillon
 */
 
 #include "server.h"
@@ -34,9 +34,27 @@ int		gui_ppo(t_server *server, t_client *client, char *arg)
   return (-1);
 }
 
-int	gui_plv(UNUSED t_server *server, UNUSED t_client *client, UNUSED char *arg)
+int		gui_plv(t_server *server, t_client *client, char *arg)
 {
-  return (0);
+  char		*msg;
+  t_client	*tmp;
+  int		id;
+
+  id = stoi(arg);
+  for (tmp = server->cl; tmp != NULL; tmp = tmp->next)
+    {
+      if (tmp->player->id == id)
+	{
+	  msg = cnprintf(BUFSIZ, "plv %d %d\n",
+			 tmp->player->id,
+			 tmp->player->level);
+	  queue_push(&client->queue, msg);
+	  free(msg);
+	  return (0);
+	}
+    }
+  queue_push(&client->queue, "sbp\n");
+  return (-1);
 }
 
 int	gui_pin(UNUSED t_server *server, UNUSED t_client *client, UNUSED char *arg)
