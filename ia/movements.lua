aa460c441e6a371a808fbcb54246d88bc2de885a
John <jonathan.collinet@epitech.eu>
2014-07-01 15:27:21 +0200

    give me the moovement for go at specific place on result of <voir>

--- ia/movements.lua
+++ ia/movements.lua
@@ -21,3 +21,22 @@ function calc_orientation(where)
 	}
 	return what_orientation[ORIENTATION][where]
 end
+
+function determine_way_to(nb)
+	local tab_avance = {1, 3, 7, 13, 21, 31, 43, 57, 73}
+	local tab_gauche = {2, 5, 6, 10, 11, 12, 17, 18, 19, 20, 26, 27, 28, 29, 30, 37, 38, 39, 40,
+						41, 42, 50, 51, 52, 53, 54, 55, 56, 65, 66, 67, 68, 69, 70, 71, 72}
+	local tab_droite = {4, 8, 9, 14, 15, 16, 22, 23, 24, 25, 32, 33, 34, 35, 36, 44, 45, 46, 47,
+						48, 49, 58, 59, 60, 61, 62, 63, 64, 74, 75, 76, 77, 78, 79, 80, 81}
+
+	if (is_match_tab(tab_avance, nb) == true) then return "avance" end
+	if (is_match_tab(tab_gauche, nb) == true) then return "gauche" end
+	if (is_match_tab(tab_droite, nb) == true) then return "droite" end
+	return nil
+end
+
+function random_moove(tcp)
+	local mooves = {"avance", "gauche", "droite"}
+	local rand = math.random(1, 3)
+	return execute_cmd(tcp, mooves[rand])
+end
