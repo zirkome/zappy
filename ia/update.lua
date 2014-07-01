f772535d46d02c2c8a4981757a3afa443001cd02
John <jonathan.collinet@epitech.eu>
2014-07-01 15:21:21 +0200

    add update_ressource_if, return me the case where is the needed ressource to levelup

--- ia/update.lua
+++ ia/update.lua
@@ -3,3 +3,15 @@ function update_ressource(tab)
 		ITEM[tab[i][1]] = tonumber(tab[i][2])
 	end
 end
+
+function update_ressource_if(tab_case)
+	for i = 1, #tab_case do
+		for j = 1, #tab_case[i] do
+			if (is_required_at(tab_case[i][j]) == true) then
+				print("i've found and am need " .. tab_case[i][j])
+				return tab_case[i][j], i
+			end
+		end
+	end
+	return false
+end
