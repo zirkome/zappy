b0a723ae59796c5bc18895a50b88f3d83aff0d6f
John <jonathan.collinet@epitech.eu>
2014-07-01 15:29:46 +0200

    change looping method on for to read ALL case in array, even is nil

--- ia/utils/utils_display.lua
+++ ia/utils/utils_display.lua
@@ -4,15 +4,15 @@ function print_bkt(title, inner)
 end
 
 function display_2dtab(tab)
-	for k, v in pairs(tab) do
-		for w, x in pairs(tab[k]) do
-			print(k, "[" .. x .. "]")
+	for k = 1, #tab do
+		for w = 1, #tab[k] do
+			print(k, "[" .. tab[k][w] .. "]")
 		end
 	end
 end
 
 function display_tab(tab)
-	for k, v in pairs(tab) do
-		print(k, v)
+	for k = 1, #tab do
+		print(k, "[" .. tab[k] .. "]")
 	end
 end
