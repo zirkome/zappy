18f4809f0138b2a47a237e0a78429d942fd3e8ab
John <jonathan.collinet@epitech.eu>
2014-07-01 15:29:02 +0200

    stop to use gmatch, parse character by character for have empty cases

--- ia/utils/utils_parse.lua
+++ ia/utils/utils_parse.lua
@@ -1,4 +1,4 @@
-function get_word(str)
+function parse_word(str)
 	local tab = {}
 	for w in str:gmatch("%w+") do
 		table.insert(tab, w)
@@ -7,21 +7,36 @@ function get_word(str)
 end
 
 function parse_case(str)
-	local tab = {{}}
-	if (str) then
-		local i = 1
-		for w in str:gmatch("%w+.") do
-			if (w:find(",") or w:find("}")) then
-				for x in w:gmatch("%w+") do
-					table.insert(tab[i], x)
-				end
-				i = i + 1;
-				table.insert(tab, {})
-			else
-				table.insert(tab[i], w:match("%a+"))
+	local number = get_nb_case_of(",", str)
+	local tab = {}
+	local j = 1
+	local k = 1
+	local dep = 1
+	local coma = false
+
+	for i = 1, number do table.insert(tab, {}) end
+	tab[1][1] = ""
+	if (str:sub(2, 2) == " ") then dep = 3 end
+	for i = dep, #str do
+		local c = str:sub(i,i)
+		if (c == ",") then
+			k = 1
+			j = j + 1
+			tab[j][k] = ""
+			if (str:sub(i + 1, i + 1) == "," or str:sub(i + 1, i + 1) == "}") then
+				tab[j][k] = "X"
+			elseif (str:sub(i + 1, i + 1) == " ") then
+				coma = true
+			end
+		elseif (c == " " and coma == false) then
+			k = k + 1
+			tab[j][k] = ""
+		else
+			if (c ~= "{" and c ~= "}" and c ~= "\n" and c ~= " ") then
+				coma = false
+				tab[j][k] = tab[j][k] .. c
 			end
 		end
 	end
-	table.remove(tab, #tab)
 	return tab
 end
