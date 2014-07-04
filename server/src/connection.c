f211eaadc30a800cb99015ff34d2134ae1dfa46c
Luc Sinet <luc.sinet@epitech.eu>
2014-07-04 14:57:50 +0200

    now spread the player's inventory when dying

--- server/src/connection.c
+++ server/src/connection.c
@@ -5,7 +5,7 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Fri May  2 22:12:56 2014 luc sinet
-** Last update Fri Jul  4 11:34:23 2014 luc sinet
+** Last update Fri Jul  4 14:19:59 2014 luc sinet
 */
 
 #include "server.h"
@@ -46,8 +46,23 @@ void		erase_client(t_world *world, t_client *cl)
   printf("Client disconnected\n");
 }
 
+void		spread_inventory(t_world *world, t_player *pl)
+{
+  unsigned int	i;
+  int		j;
+
+  if (pl == NULL)
+    return ;
+  for (i = 0; i < sizeof(pl->inventory) / sizeof(int); ++i)
+    {
+      for (j = 0; j < pl->inventory[i]; ++j)
+	add_to_world(world, i + FOOD, pl->x, pl->y);
+    }
+}
+
 int		kick_user(t_list *list, t_client *cl, t_world *world)
 {
+  spread_inventory(world, cl->player);
   erase_client(world, cl);
   list_del_node(list, cl);
   return (2);
