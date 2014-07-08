eb7b029cad0cbf0a9c5f077fd0ad3d19418a7f6b
Guillaume Fillon <guillaume@posteo.de>
2014-07-08 16:27:25 +0200

    Implemented Fork (start/end), Egg (Hatch/Rotten), Player's death and team win

---
+++ server/src/gui_egg.c
@@ -0,0 +1,26 @@
+/*
+** gui_egg_born.c for zappy in /home/kokaz/projects/tek2/unix/zappy/server
+**
+** Made by guillaume fillon
+** Login   <kokaz@epitech.net>
+**
+** Started on  Fri Jul  4 16:00:50 2014 guillaume fillon
+** Last update Tue Jul  8 16:24:11 2014 guillaume fillon
+*/
+
+#include "server.h"
+#include "gui.h"
+
+int	gui_egg_born(UNUSED t_server *server, t_client *graphic,
+		     UNUSED t_client *client, t_gui_arg *arg)
+{
+  queue_push_message(&graphic->queue, "eht %d\n", arg->id);
+  return (0);
+}
+
+int	gui_egg_dead(UNUSED t_server *server, t_client *graphic,
+		     UNUSED t_client *client, t_gui_arg *arg)
+{
+  queue_push_message(&graphic->queue, "edi %d\n", arg->id);
+  return (0);
+}
